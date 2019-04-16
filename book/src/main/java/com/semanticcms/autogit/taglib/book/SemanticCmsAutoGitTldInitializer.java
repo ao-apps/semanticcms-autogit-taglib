9a32c339643a1229e54191436accff91d8165993
AO Industries, Inc <support@aoindustries.com>
2019-04-15 20:51:44 -0500

    Using Maven property "documented.javac.link.javaApi" to link to the correct Java API version

--- book/src/main/java/com/semanticcms/autogit/taglib/book/SemanticCmsAutoGitTldInitializer.java
+++ book/src/main/java/com/semanticcms/autogit/taglib/book/SemanticCmsAutoGitTldInitializer.java
@@ -42,7 +42,7 @@ public class SemanticCmsAutoGitTldInitializer extends TagReferenceInitializer {
 			"Taglib Reference",
 			"/autogit/taglib",
 			"/semanticcms-autogit.tld",
-			Maven.properties.getProperty("javac.link.javaApi.jdk7"),
+			Maven.properties.getProperty("documented.javac.link.javaApi"),
 			Maven.properties.getProperty("javac.link.javaeeApi.6"),
 			additionalApiLinks
 		);
