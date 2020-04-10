35f63e91f8bb804921c99bd6f93732771e856818
AO Industries, Inc <support@aoindustries.com>
2020-04-10 17:51:14 -0500

    Fixed API links.

--- book/src/main/java/com/semanticcms/autogit/taglib/book/SemanticCmsAutoGitTldInitializer.java
+++ book/src/main/java/com/semanticcms/autogit/taglib/book/SemanticCmsAutoGitTldInitializer.java
@@ -53,7 +53,7 @@ public class SemanticCmsAutoGitTldInitializer extends TagReferenceInitializer {
 				Path.valueOf("/semanticcms-autogit.tld")
 			),
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
-			Maven.properties.getProperty("javadoc.link.javaee.6"),
+			Maven.properties.getProperty("documented.javadoc.link.javaee"),
 			additionalApiLinks
 		);
 	}
