e900c56eb793d58d8e0ead925ed366b485e10540
AO Industries, Inc <support@aoindustries.com>
2020-05-30 00:50:09 -0500

    New option "requireLinks" that will fail when a class does not map to a
    package in "apiLinks".
    Defaults to "false".

--- book/src/main/java/com/semanticcms/autogit/taglib/book/SemanticCmsAutoGitTldInitializer.java
+++ book/src/main/java/com/semanticcms/autogit/taglib/book/SemanticCmsAutoGitTldInitializer.java
@@ -44,6 +44,7 @@ public class SemanticCmsAutoGitTldInitializer extends TagReferenceInitializer {
 				),
 				Path.valueOf("/semanticcms-autogit.tld")
 			),
+			true,
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
 			Maven.properties.getProperty("documented.javadoc.link.javaee"),
 			// Self
